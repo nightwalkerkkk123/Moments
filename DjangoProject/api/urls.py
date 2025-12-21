a490d07904c0de8da5c8beea8c85746d1cd91387
Ast-fish <3386354609@qq.com>
2025-12-22 03:46:00 +0800

    feat搜索:初步实现了搜索部分的9个接口，更改了api中的models、serializers、views、urls四个文件。不过由于找不到搜索页面，因此还未做测试

--- DjangoProject/api/urls.py
+++ DjangoProject/api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, login_view, logout_view
 from .utils import health_check, api_info
-
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 
@@ -13,3 +12,22 @@ urlpatterns = [
     path('auth/logout/', logout_view, name='logout'),
     path('', include(router.urls)),
 ]
+
+
+###  搜索部分新增url（共⑨个）  ###
+from .views import (
+    SearchView, HotTagsView, SearchSuggestionsView,
+    SearchHistorySaveView, SearchHistoryListView, SearchHistoryClearView
+)
+urlpatterns += [
+    path('search', SearchView.as_view(), name='search'),
+    path('tags/hot', HotTagsView.as_view(), name='hot-tags'),
+    path('search/suggestions', SearchSuggestionsView.as_view(), name='suggestions'),
+    path('search/history', SearchHistorySaveView.as_view(), name='save-history'),
+    path('search/history/list', SearchHistoryListView.as_view(), name='list-history'),
+    path('search/history/clear', SearchHistoryClearView.as_view(), name='clear-history'),
+    path('posts/<int:pk>/like', PostLikeView.as_view(), name='post-like'),
+    path('posts/<int:pk>/comments', PostCommentListView.as_view(), name='post-comment-list'),
+    path('posts/<int:pk>/comments/add', PostCommentCreateView.as_view(), name='post-comment-add'),
+]
+####################

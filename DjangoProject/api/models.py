6c5de07e43e8ff48d3bbc2ca0e08736caa034ecb
Ast-fish <3386354609@qq.com>
2025-12-23 22:27:16 +0800

    refactor/test:搜索

--- DjangoProject/api/models.py
+++ DjangoProject/api/models.py
@@ -2,7 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-####################  搜索部分开始  ######################
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
--- DjangoProject/api/serializers.py
+++ DjangoProject/api/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Post, SearchHistory    # 搜索部分的引用
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
